30b25ab1c9ae7c162348983aac63a7562d4478ee
AlejoMl1 <a.munoz254@uniandes.edu.co>
2022-02-08 15:51:12 -0500

    force false , deply succesfully

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { conn } = require('./src/db.js');
 require('dotenv').config();
 const PORT = process.env.PORT 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`) ; // eslint-disable-line no-console
   });
